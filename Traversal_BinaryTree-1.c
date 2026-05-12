3e96a5f5c4e1745ed51863ec06352f1cc58221c2
vivekkashid123 <vivekkashid995@gmail.com>
2026-05-12 15:23:53 +0530

    Add files via upload

---
+++ Traversal_BinaryTree-1.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Preorder Traversal
+void preorder(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+// Inorder Traversal
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+// Postorder Traversal
+void postorder(struct Node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    // Creating Binary Tree manually
+    /*
+            1
+           / \
+          2   3
+         / \   \
+        4   5   6
+    */
+
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->right->right = createNode(6);
+
+    printf("Preorder Traversal: ");
+    preorder(root);
+
+    printf("\nInorder Traversal: ");
+    inorder(root);
+
+    printf("\nPostorder Traversal: ");
+    postorder(root);
+
+    return 0;
+}
