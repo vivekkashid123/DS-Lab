1cca3bba2cc401a4d89e6d2f0495ca84d78db417
vivekkashid123 <vivekkashid995@gmail.com>
2026-05-12 15:25:30 +0530

    Add files via upload

---
+++ Traversal_BinaryTree-1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+// Node class
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Preorder Traversal
+void preorder(Node* root) {
+    if (root != NULL) {
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+// Inorder Traversal
+void inorder(Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+// Postorder Traversal
+void postorder(Node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+int main() {
+    // Creating Binary Tree manually
+    /*
+            1
+           / \
+          2   3
+         / \   \
+        4   5   6
+    */
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+
+    cout << "Preorder Traversal: ";
+    preorder(root);
+
+    cout << "\nInorder Traversal: ";
+    inorder(root);
+
+    cout << "\nPostorder Traversal: ";
+    postorder(root);
+
+    return 0;
+}
